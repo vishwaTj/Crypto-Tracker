afc26f4a3a22f0a789e8fbb678208c0d5ab027f4
Tj_ <tjvishwa88@gmail.com>
2023-05-01 07:36:46 +0530

    Firebase created and and most of the config part added to code

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCMGqIEpMx1sG0860gEpVg_BxYF44xoi_k",
+    authDomain: "crypto-tracker-283ed.firebaseapp.com",
+    projectId: "crypto-tracker-283ed",
+    storageBucket: "crypto-tracker-283ed.appspot.com",
+    messagingSenderId: "795156891791",
+    appId: "1:795156891791:web:a4dfd4907a913af68d48fa"
+  };
+
+  export default firebaseConfig;
